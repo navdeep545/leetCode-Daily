7efc4d9806a53fa58c4be6b38fe9cbe349d3322f
LadyDhaga <navdeepjaj016@gmail.com>
2023-02-05 12:58:32 +0530

    nyc question, done by myself

---
+++ 86. Partition List.cpp
@@ -0,0 +1,33 @@
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        ListNode *low = new ListNode(x-1) ;
+        ListNode *high = new ListNode(x+1) ;
+        auto lower = low ;
+        auto higher = high ;
+        // auto temp = head ;
+        while(head){
+            if(head->val < x){
+                lower->next = head ;
+                lower = lower->next ;
+            }
+            else {
+                higher->next = head ;
+                higher = higher->next ;
+            }
+            head = head->next ;
+        }
+        lower->next = high->next ;
+        higher->next = nullptr ;
+        return low->next  ;
+    }
+};
+
